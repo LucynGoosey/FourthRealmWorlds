2e98855fad8ae3b40c6c2c1e59c49b22e001319d
LucynGoosey <didgeridev@gmail.com>
2024-07-05 23:42:49 -0400

    added world list

--- src/main/java/me/lucyn/fourthrealm/FourthRealmWorlds.java
+++ src/main/java/me/lucyn/fourthrealm/FourthRealmWorlds.java
@@ -1,14 +1,29 @@
 package me.lucyn.fourthrealm;
 
+import org.bukkit.World;
 import org.bukkit.plugin.java.JavaPlugin;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public final class FourthRealmWorlds extends JavaPlugin {
 
     public static FourthRealmCore fourthRealmCore;
+    public static List<World> worlds;
 
     @Override
     public void onEnable() {
         fourthRealmCore = (FourthRealmCore) this.getServer().getPluginManager().getPlugin("FourthRealmCore");
+        worlds = new ArrayList<>();
+
+        List<World> allWorlds = getServer().getWorlds();
+
+        for(World world : allWorlds) {
+            if(world.getEnvironment().equals(World.Environment.NORMAL)) {
+                worlds.add(world);
+
+            }
+        }
 
 
 
--- src/main/java/me/lucyn/fourthrealm/listeners/BedListener.java
+++ src/main/java/me/lucyn/fourthrealm/listeners/BedListener.java
@@ -22,6 +22,7 @@ public class BedListener implements Listener {
             fourthRealmCore.setPlayerData(player);
 
 
+
         }
 
     }
